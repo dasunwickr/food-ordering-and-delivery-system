24a75cfcb2ad881b50fd21185df0b1b1234a592b
Kavindi Pathiraja <kavindipathiraja117@gmail.com>
2025-03-29 17:01:11 +0530

    feat:be(review): create review repository

---
+++ backend/ratings-and-reviews-service/src/main/java/com/nomnom/ratings_and_reviews_service/repository/ReviewRepository.java
@@ -0,0 +1,12 @@
+package com.nomnom.ratings_and_reviews_service.repository;
+
+import com.nomnom.ratings_and_reviews_service.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByTargetIdAndTargetType(Long targetId, Review.TargetType targetType);
+}
