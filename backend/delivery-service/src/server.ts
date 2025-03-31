22afcee8e16a867725805b831d04c1d91621ba38
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-03-31 13:42:34 +0530

    chore:be(delivery): update the db util

--- backend/delivery-service/src/server.ts
+++ backend/delivery-service/src/server.ts
@@ -1,12 +1,5 @@
-import express, { Request, Response } from "express";
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express!");
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- backend/delivery-service/src/utils/db.util.ts
+++ backend/delivery-service/src/utils/db.util.ts
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
-const connectToDatabase = async () => {
-  const MONGO_URI = process.env.MONGO_URI;
 
-  if (!MONGO_URI) {
-    console.error("MongoDB URI is not defined in the environment variables.");
-    return;
-  }
+const MONGO_URI = process.env.MONGO_URI;
 
+if (!MONGO_URI) {
+  console.error("MongoDB URI is not defined in the environment variables.");
+  process.exit(1);
+}
+
+export const connectToDatabase = async (): Promise<void> => {
   try {
     await mongoose.connect(MONGO_URI);
-    console.log("Connected to MongoDB");
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error("Error connecting to database:", error);
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
   }
 };
-
-export default connectToDatabase;
