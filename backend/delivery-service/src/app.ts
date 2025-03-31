b4269ae7f99b17ea9b95c4c5a93ef30b0e79b6a8
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-03-31 13:47:34 +0530

    chore:be(delivery): update the app + server

--- backend/delivery-service/src/app.ts
+++ backend/delivery-service/src/app.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { connectToDatabase } from "./utils/db.util";
 
 const app = express();
 
+// Middlewarea
 app.use(express.json());
 app.use(cors());
+
+// Database Connection
+connectToDatabase();
+
+export default app;
--- backend/delivery-service/src/server.ts
+++ backend/delivery-service/src/server.ts
@@ -1,4 +1,6 @@
-const PORT = process.env.PORT || 3000;
+import app from "./app";
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
