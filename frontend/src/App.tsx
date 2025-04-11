1cf18e32efe6ea3e96e10a5cac70456d1475ae0f
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-04-11 07:19:41 +0530

    config(fe): add tanstack table + tanskatck query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "./components/ui/button";
+import { Textarea } from "./components/ui/textarea";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button>Hello</Button>
+      <Textarea></Textarea>
+    </div>
+  );
 };
 
 export default App;
