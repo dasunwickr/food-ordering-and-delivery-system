80cb14b663716f8c126c4bff4cbe2fa9238d042f
Kavindi Pathiraja <kavindipathiraja117@gmail.com>
2025-04-13 13:45:41 +0530

    feat:be(order): create OrderController

---
+++ backend/order-service/src/main/java/com/nomnom/order_service/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.nomnom.order_service.controller;
+
+import com.nomnom.order_service.dto.OrderDTO;
+import com.nomnom.order_service.request.CreateOrderRequest;
+import com.nomnom.order_service.service.IOrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+    private final IOrderService orderService;
+
+    public OrderController(IOrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<OrderDTO> createOrder(@RequestBody CreateOrderRequest request) {
+        return ResponseEntity.ok(orderService.createOrder(request));
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderDTO> getOrderById(@PathVariable String orderId) {
+        return ResponseEntity.ok(orderService.getOrderById(orderId));
+    }
+
+    @PutMapping("/update-status/{orderId}")
+    public ResponseEntity<Void> updateOrderStatus(@PathVariable String orderId, @RequestParam String status) {
+        orderService.updateOrderStatus(orderId, status);
+        return ResponseEntity.noContent().build();
+    }
+}
