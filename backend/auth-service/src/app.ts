fe4c4a9f5a2ba08d70087b6328418b5460fecf56
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-04-13 08:49:16 +0530

    config:be(auth): reset the auth project

--- backend/auth-service/src/app.ts
+++ backend/auth-service/src/app.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.route';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use('/auth', authRoutes);
+// app.use('/auth', authRoutes);
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/nomnom-auth');
+mongoose.connect(process.env.MONGO_URI!);
 
 export default app;
--- backend/auth-service/src/routes/auth.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { signUpHandler, loginHandler } from '../controllers/auth.controller';
-
-const router = Router();
-
-router.post('/signup', signUpHandler);
-router.post('/login', loginHandler);
-
-export default router;
