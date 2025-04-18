89e47d87f4a56c584a1f247472df6df11452d933
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-04-18 09:03:40 +0530

    chore:be(auth): update UserRepository to add findByEmail, findByUserType, findByUserTypeAndUserStatus, existsByEmail

--- backend/user-service/src/main/java/com/nomnom/user_service/repository/UserRepository.java
+++ backend/user-service/src/main/java/com/nomnom/user_service/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.nomnom.user_service.repository;
 
 
+import com.nomnom.user_service.enums.UserStatus;
+import com.nomnom.user_service.enums.UserType;
 import com.nomnom.user_service.model.User;
 import com.nomnom.user_service.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,10 +11,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
-
-
-
+    List<User> findByUserType(UserType userType);
+    List<User> findByUserTypeAndUserStatus(UserType userType, UserStatus userStatus);
+    boolean existsByEmail(String email);
 
 }
