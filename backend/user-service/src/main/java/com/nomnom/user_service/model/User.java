9e158ea4ca5038edaebdb085166b5eee5ff138eb
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-04-18 07:40:26 +0530

    chore:be(auth): update user class logic + add validation

--- backend/user-service/src/main/java/com/nomnom/user_service/model/User.java
+++ backend/user-service/src/main/java/com/nomnom/user_service/model/User.java
@@ -4,55 +4,60 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.nomnom.user_service.enums.UserStatus;
+import com.nomnom.user_service.enums.UserType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 @Data
-@Getter
-@Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
         include = JsonTypeInfo.As.PROPERTY,
         property = "userType"
 )
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = Admin.class, name = "admin"),
-        @JsonSubTypes.Type(value = Customer.class, name = "customer"),
-        @JsonSubTypes.Type(value = Driver.class, name = "driver"),
-        @JsonSubTypes.Type(value = RestaurantOwner.class, name = "restaurantOwner")
+        @JsonSubTypes.Type(value = Admin.class, name = "ADMIN"),
+        @JsonSubTypes.Type(value = Customer.class, name = "CUSTOMER"),
+        @JsonSubTypes.Type(value = Driver.class, name = "DELIVERY_DRIVER"),
+        @JsonSubTypes.Type(value = Restaurant.class, name = "RESTAURANT")
 })
 
 public abstract class User {
     @Id
     private String id;
 
+    @NotNull
+    @Size(min = 1, max = 50)
     private String firstName;
 
+    @NotNull
+    @Size(min = 1, max = 50)
     private String lastName;
 
-    private String address;
-
-    private Double longitude;
-
-
-    private Double latitude;
-
-    @JsonProperty("email")
+    @NotNull
+    @Email
     private String email;
 
-    @JsonProperty("contactNumber")
+    @NotNull
+    @Size(min = 10, max = 15)
     private String contactNumber;
 
-    @JsonProperty("username")
-    private String username;
-
-    @JsonProperty("password") // Allow write-only access
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    @JsonProperty("userType") // Explicitly store userType in MongoDB
-    private String userType;
+    @JsonProperty("userType")
+    private UserType userType;
+
+    private UserStatus userStatus = UserStatus.OFFLINE;
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
 }
