5c655a37357bb17a66087797a0d02b701b71f80d
Dhanuka Navodya <162307420+DhanukaNavodya@users.noreply.github.com>
2025-04-22 10:06:52 +0530

    feat:fe(menu):frontend menu adding form

---
+++ frontend/src/app/menu/menuItems/page.tsx
@@ -0,0 +1,12 @@
+import MenuItems from '@/components/common/menu/menuItems/MenuItems';
+import React from 'react';
+
+function Page() {
+  return (
+    <div>
+      <MenuItems/>
+    </div>
+  );
+}
+
+export default Page;
---
+++ frontend/src/components/common/menu/menuItems/MenuItems.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MenuItems() {
+  return (
+    <div>
+      MenuItems
+    </div>
+  );
+}
+
+export default MenuItems;
