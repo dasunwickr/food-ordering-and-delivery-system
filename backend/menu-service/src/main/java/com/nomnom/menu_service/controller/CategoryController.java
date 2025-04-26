141737aa915a4e007fad197d09e755ab656fd3ff
Dhanuka Navodya <162307420+DhanukaNavodya@users.noreply.github.com>
2025-04-26 09:58:51 +0530

    chore:be(menu):add image to category

--- backend/menu-service/src/main/java/com/nomnom/menu_service/controller/CategoryController.java
+++ backend/menu-service/src/main/java/com/nomnom/menu_service/controller/CategoryController.java
@@ -5,8 +5,11 @@ import com.nomnom.menu_service.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
@@ -15,13 +18,14 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * Add a new category using the Category model directly.
+     * Add a new category with an image.
      */
-    @PostMapping(consumes = "application/json")
-    public ResponseEntity<Category> addCategory(@RequestBody Category category) {
-        // Call the service layer to add the category
-        Category savedCategory = categoryService.addCategory(category.getName());
-        return ResponseEntity.ok(savedCategory);
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<Category> addCategory(
+            @RequestParam("name") String categoryName,
+            @RequestParam("image") MultipartFile imageFile) throws IOException {
+        Category category = categoryService.addCategory(categoryName, imageFile);
+        return ResponseEntity.ok(category);
     }
 
     /**
