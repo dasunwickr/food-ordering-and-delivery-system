d1d4577a32d1980cb46979f00848016aac1a233b
Dhanuka Navodya <162307420+DhanukaNavodya@users.noreply.github.com>
2025-05-03 17:52:22 +0530

    chore:fe(customer):update customer page

--- frontend/src/app/customer/page.tsx
+++ frontend/src/app/customer/page.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import RestaurantsSlider from "@/components/common/restaurants/restaurantSlider";
+import DisplayCategories from "@/components/common/categories/categories";
+import MenuItemsWithOffers from "@/components/customer/menu-item-offers";
 
 function Page() {
   return (
     <div>
-     page
+     <MenuItemsWithOffers/>
+      <DisplayCategories/>
+      <RestaurantsSlider/>
+      
+
     </div>
   );
 }
